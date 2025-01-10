79df4137efcc392a34f1eeb12495dece960eabfc
ankit-patel-developer <ankitjpatel2007@gmail.com>
2025-01-09 18:56:20 -0600

    adding-components

---
+++ src/components/Account_Create/Account_Create.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Account_Create = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Account_Create
---
+++ src/components/Account_Edit/Account_Edit.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Account_Edit = () => {
+  return <div></div>;
+};
+
+export default Account_Edit;
